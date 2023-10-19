c331d89e62e9ccc205bba4962f85661eef73dd7f
Chance Ludwick <chanceludwick@gmail.com>
2023-10-19 09:39:15 -0500

    Refactor to useReducer, for funsies

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,23 +1,39 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+type LightState = {
+  red: number;
+  yellow: number;
+  green: number;
+};
+
+type LightAction = { type: 'NEXT' };
+
+const initialState = {
+  red: 1,
+  yellow: 0,
+  green: 0,
+};
+
+const lightReducer = (state: LightState, action: LightAction) => {
+  switch (action.type) {
+    case 'NEXT':
+      if (state.red === 1) {
+        return { red: 0, yellow: 0, green: 1 };
+      } else if (state.yellow === 1) {
+        return { red: 1, yellow: 0, green: 0 };
+      } else {
+        return { red: 0, yellow: 1, green: 0 };
+      }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
 
 export const FunctionalTrafficLight = () => {
-  const [lights, setLights] = useState({
-    red: 1,
-    yellow: 0,
-    green: 0,
-  });
+  const [lights, dispatch] = useReducer(lightReducer, initialState);
 
   const nextState = () => {
-    setLights((prevState) => {
-      const { red, yellow, green } = prevState;
-      if (red === 1) {
-        return { ...prevState, red: 0, green: 1 };
-      } else if (yellow === 1) {
-        return { ...prevState, red: 1, yellow: 0 };
-      } else {
-        return { ...prevState, yellow: 1, green: 0 };
-      }
-    });
+    dispatch({ type: 'NEXT' });
   };
 
   return (
