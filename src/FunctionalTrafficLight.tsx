16a044411bec76ef790227590c200f95341c8133
Chance Ludwick <chanceludwick@gmail.com>
2023-10-19 09:45:01 -0500

    Put useState version back in comments

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -30,6 +30,28 @@ const lightReducer = (state: LightState, action: LightAction) => {
 };
 
 export const FunctionalTrafficLight = () => {
+  // ? Solved this in useState, but decided to use useReducer to practice it
+
+  // const [lights, setLights] = useState({
+  //   red: 1,
+  //   yellow: 0,
+  //   green: 0,
+  // });
+
+  // const nextState = () => {
+  //   setLights((prevState) => {
+  //     const { red, yellow, green } = prevState;
+  //     if (red === 1) {
+  //       return { ...prevState, red: 0, green: 1 };
+  //     } else if (yellow === 1) {
+  //       return { ...prevState, red: 1, yellow: 0 };
+  //     } else {
+  //       return { ...prevState, yellow: 1, green: 0 };
+  //     }
+  //   });
+  // };
+
+  // useReducer version
   const [lights, dispatch] = useReducer(lightReducer, initialState);
 
   const nextState = () => {
